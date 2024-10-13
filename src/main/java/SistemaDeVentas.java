db292380798a3be995dc07e8c7f7bbed575c96d4
Héctor Monagas <monagasrr@gmail.com>
2024-10-13 17:22:07 -0400

    Init github

---
+++ src/main/java/SistemaDeVentas.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author Héctor Monagas
+ */
+public class SistemaDeVentas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
